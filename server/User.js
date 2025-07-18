c26a604ce84425a0fea3a10eb9fa9113bfde0a91
Vrinda <apple@Vrindas-MacBook-Pro.local>
2025-07-18 16:12:29 +0530

    user schema updated

--- server/User.js
+++ server/User.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     _id: { type: mongoose.Schema.Types.ObjectId, auto: true },  
-  points: { type: Number, required: true },
+  points: { type: Number, required: true, default: 0 },
   email: { type: String },
   password: { type: String },
-  tasks: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Task' }],
-  rewards: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Reward' }]
+  tasks: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Task', required: true }],
+  rewards: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Reward', required: true }]
 }, { timestamps: true });
 
 const User = mongoose.model('User', userSchema);
